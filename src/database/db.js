da0da1d851daff5828893780f2f356a7c10dc14d
idop <ido.postelnik@spotinst.com>
2020-02-16 18:13:40 +0200

    [src] Set dog-book DB

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,15 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('dog-book', 'root', 'Idospotinst23', {
-  host: 'localhost',
-  port: '',
+//const sequelize = new Sequelize('dog-book', 'root', 'Idospotinst23', {
+//  host: 'localhost',
+//  port: '',
+//  dialect: 'mysql',
+//  operatorsAliases: false
+//});
+
+const sequelize = new Sequelize('dog-book', 'admin', 'dogbook123', {
+  host: 'dogbook-db-instance-new.cs6itidygcss.eu-west-1.rds.amazonaws.com',
+  port: '3306',
   dialect: 'mysql',
   operatorsAliases: false
 });
